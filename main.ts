05470dfcd283f6e42104ff8435bdc9aa729d0cb1
nozomiii-xd <nozomiii273@gmail.com>
2026-01-14 20:38:59 +0800

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -2,8 +2,9 @@ let strip = neopixel.create(DigitalPin.P0, 1, NeoPixelMode.RGB)
 strip.setBrightness(40)
 
 basic.forever(function () {
-    for (let hue = 0; hue <= 360; hue += 1) {
+    for (let hue = 0; hue <= 360; hue++) {
         strip.showColor(neopixel.hsl(hue, 100, 50))
         basic.pause(20)
     }
 })
+
